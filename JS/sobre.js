aa6d488accc58cc033cc51103828e8b5ee55ce87
Arthurbot02 <155775801+Arthurbot02@users.noreply.github.com>
2024-01-10 17:56:02 -0300

    teste2 subindo arquivo

---
+++ JS/sobre.js
@@ -0,0 +1,71 @@
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+    faq.addEventListener("click", () => {
+        faq.classList.toggle("active");
+    });
+    });
+    const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
+
+    function getDistanceFromTheTop(element) {
+      const id = element.getAttribute("href");
+      return document.querySelector(id).offsetTop;
+    }
+    
+    // function nativeScroll(distanceFromTheTop) {
+    //   window.scroll({
+    //     top: distanceFromTheTop,
+    //     behavior: "smooth",
+    //   });
+    // }
+    
+    function scrollToSection(event) {
+      event.preventDefault();
+      const distanceFromTheTop = getDistanceFromTheTop(event.target) - 90;
+      smoothScrollTo(0, distanceFromTheTop);
+    }
+    
+    menuLinks.forEach((link) => {
+      link.addEventListener("click", scrollToSection);
+    });
+    
+    function smoothScrollTo(endX, endY, duration) {
+      const startX = window.scrollX || window.pageXOffset;
+      const startY = window.scrollY || window.pageYOffset;
+      const distanceX = endX - startX;
+      const distanceY = endY - startY;
+      const startTime = new Date().getTime();
+    
+      duration = typeof duration !== "undefined" ? duration : 400;
+    
+      const easeInOutQuart = (time, from, distance, duration) => {
+        if ((time /= duration / 2) < 1)
+          return (distance / 2) * time * time * time * time + from;
+        return (-distance / 2) * ((time -= 2) * time * time * time - 2) + from;
+      };
+    
+      const timer = setInterval(() => {
+        const time = new Date().getTime() - startTime;
+        const newX = easeInOutQuart(time, startX, distanceX, duration);
+        const newY = easeInOutQuart(time, startY, distanceY, duration);
+        if (time >= duration) {
+          clearInterval(timer);
+        }
+        window.scroll(newX, newY);
+      }, 1000 / 60);
+    }
+    let count =1;
+document.getElementById("radio1").checked = true;
+ 
+setInterval(function(){
+    nextImage(); 
+},5000)
+
+function nextImage (){
+    count++;
+    if(count>4){
+        count = 1;
+    }
+
+    document.getElementById("radio"+count).checked = true
+} 
